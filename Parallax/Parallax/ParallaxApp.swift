c431e1371174c78c42df7837447cda5677f944b4
luthfi1 <68719137+luthfi1@users.noreply.github.com>
2021-01-04 10:51:07 +0700

    Add files via upload

---
+++ Parallax/Parallax/ParallaxApp.swift
@@ -0,0 +1,24 @@
+//
+//  ParallaxApp.swift
+//  Parallax
+//
+//  Created by muhammad Luthfi farizqi on 01/01/21.
+//  Copyright © 2021 muhammad Luthfi farizqi . All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ParallaxApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
